118599ad8946856f472e39984c170c9690d6073a
Louis Ronald <louisronsonronald@gmail.com>
2021-06-22 01:14:39 +1000

    Added basic functions for zoom meeting manipulation into zoom-meeting.php

--- zoom-meeting.php
+++ zoom-meeting.php
@@ -2,6 +2,73 @@
 
 
 
-class ZoomMeeting {
+class ZoomMeetingAdapter {
+
+
+
+    public static function listMeetings($hostUserID) {
+        /*
+            Lists all meetings organized by the host user.
+
+            This function calls the REST API endpoint documented
+            at https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetings.
+        */
+
+    }
+
+    
+
+    public static function getMeeting($meetingID) {
+        /*
+            Gets the details of a particular meeting.
+
+            This function calls the REST API endpoint documented
+            at https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meeting.
+        */
+    }
+
+
+
+    public static function getMeetingInvitation($meetingID) {
+        /*
+            Get a meeting invitation
+
+            This function calls the REST API endpoint documented
+            at https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetinginvitation.
+        */
+    }
+
+
+
+
+    public static function listMeetingRegistrants($meetingID) {
+        /*
+            Lists all the registrants of a meeting.
+
+            This function calls the REST API endpoint documented
+            at https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetingregistrants.
+        */
+    }
+
+
+    public static function addMeetingRegistrant($meetingID) {
+        /*
+            Adds a new meeting registrant
+
+            This function calls the REST API endpoint documented
+            at https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetingregistrantcreate.
+        */
+    }
+
+
+    public static function deleteMeetingRegistrant($meetingID) {
+        /*
+            Deletes a new meeting registrant
+
+            This function calls the REST API endpoint documented
+            at https://marketplace.zoom.us/docs/api-reference/zoom-api/meetings/meetingregistrantdelete.
+        */
+    }
+
 
 }
