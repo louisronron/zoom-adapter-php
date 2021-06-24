3ecb08ee9f4ac190078cba68352d6f394981eb93
Louis Ronald <louisronsonronald@gmail.com>
2021-06-24 15:11:41 +1000

    Removed API example response comments for each function

--- zoom-meeting.php
+++ zoom-meeting.php
@@ -29,57 +29,6 @@ class ZoomMeetingAdapter {
             $array = json_decode(json_encode($curl->response), true);
             return $array["id"];
         }
-
-
-
-        /*
-
-
-        Example API Response
-
-        
-        {
-            "created_at": "2018-11-15T01:10:08Z",
-            "custom_attributes": [
-              {
-                "key": "cb3674544gexq",
-                "name": "Country of Citizenship",
-                "value": "Nepal"
-              }
-            ],
-            "id": "z8dsdsdsdsdCfp8uQ",
-            "first_name": "Harry",
-            "last_name": "Grande",
-            "email": "harryg@dfkjdslfjkdsfjkdsf.fsdfdfd",
-            "type": 2,
-            "role_name": "Owner",
-            "pmi": 100000000,
-            "use_pmi": false,
-            "personal_meeting_url": "https://zoom.us/j/6352635623323434343443",
-            "timezone": "America/Los_Angeles",
-            "verified": 1,
-            "dept": "",
-            "last_login_time": "2019-09-13T21:08:52Z",
-            "last_client_version": "4.4.55383.0716(android)",
-            "pic_url": "https://lh4.googleusercontent.com/-hsgfhdgsfghdsfghfd-photo.jpg",
-            "host_key": "0000",
-            "jid": "hghghfghdfghdfhgh@xmpp.zoom.us",
-            "group_ids": [],
-            "im_group_ids": [
-              "CcSAAAAAAABBBVoQ"
-            ],
-            "account_id": "EAAAAAbbbbbCCCCHMA",
-            "language": "en-US",
-            "phone_country": "USA",
-            "phone_number": "00000000",
-            "status": "active",
-            "role_id": "hdsfwyteg3675hgfs",
-            "manager": "name@example.com"
-          }
-
-        */
-
-        
     }
 
 
@@ -115,76 +64,6 @@ class ZoomMeetingAdapter {
             $array = json_decode(json_encode($curl->response), true);
             return $array;
         }
-
-
-        /*
-
-        Example API Response
-
-
-        {
-        "page_count": 1,
-        "page_number": 1,
-        "page_size": 30,
-        "total_records": 4,
-        "meetings": [
-            {
-            "uuid": "mlghmfghlBBB",
-            "id": 11111,
-            "host_id": "abckjdfhsdkjf",
-            "topic": "Zoom Meeting",
-            "type": 2,
-            "start_time": "2019-08-16T02:00:00Z",
-            "duration": 30,
-            "timezone": "America/Los_Angeles",
-            "created_at": "2019-08-16T01:13:12Z",
-            "join_url": "https://zoom.us/j/11111"
-            },
-            {
-            "uuid": "J8H8eavweUcd321==",
-            "id": 2222,
-            "host_id": "abckjdfhsdkjf",
-            "topic": "TestMeeting",
-            "type": 2,
-            "start_time": "2019-08-16T19:00:00Z",
-            "duration": 60,
-            "timezone": "America/Los_Angeles",
-            "agenda": "RegistrationDeniedTest",
-            "created_at": "2019-08-16T18:30:46Z",
-            "join_url": "https://zoom.us/j/2222"
-            },
-            {
-            "uuid": "SGVTAcfSfCbbbb",
-            "id": 33333,
-            "host_id": "abckjdfhsdkjf",
-            "topic": "My Meeting",
-            "type": 2,
-            "start_time": "2019-08-16T22:00:00Z",
-            "duration": 60,
-            "timezone": "America/Los_Angeles",
-            "created_at": "2019-08-16T21:15:56Z",
-            "join_url": "https://zoom.us/j/33333"
-            },
-            {
-            "uuid": "64123avdfsMVA==",
-            "id": 44444,
-            "host_id": "abckjdfhsdkjf",
-            "topic": "MyTestPollMeeting",
-            "type": 2,
-            "start_time": "2019-08-29T18:00:00Z",
-            "duration": 60,
-            "timezone": "America/Los_Angeles",
-            "created_at": "2019-08-29T17:32:33Z",
-            "join_url": "https://zoom.us/j/4444"
-            }
-        ]
-        }
-
-
-        */
-
-
-
     }
 
     
@@ -219,83 +98,6 @@ class ZoomMeetingAdapter {
             return $array;
         }
 
-
-
-
-        /*
-
-
-        Example API Response
-
-
-        {
-        "agenda": "API overview",
-        "created_at": "2019-09-09T15:54:24Z",
-        "duration": 60,
-        "host_id": "ABcdofjdogh11111",
-        "id": 1234555466,
-        "join_url": "https://zoom.us/j/1234555466",
-        "settings": {
-            "alternative_hosts": "kjxckfjxgfgjdfk@dkjfhdskhf.com",
-            "approval_type": 2,
-            "audio": "both",
-            "auto_recording": "local",
-            "close_registration": false,
-            "cn_meeting": false,
-            "enforce_login": false,
-            "enforce_login_domains": "mycompanydomain.com",
-            "global_dial_in_countries": [
-            "US"
-            ],
-            "global_dial_in_numbers": [
-            {
-                "city": "New York",
-                "country": "US",
-                "country_name": "US",
-                "number": "+1 000011100",
-                "type": "toll"
-            },
-            {
-                "city": "San Jose",
-                "country": "US",
-                "country_name": "US",
-                "number": "+1 6699006833",
-                "type": "toll"
-            },
-            {
-                "city": "San Jose",
-                "country": "US",
-                "country_name": "US",
-                "number": "+1 221122112211",
-                "type": "toll"
-            }
-            ],
-            "host_video": false,
-            "in_meeting": false,
-            "join_before_host": true,
-            "mute_upon_entry": false,
-            "participant_video": false,
-            "registrants_confirmation_email": true,
-            "use_pmi": false,
-            "waiting_room": false,
-            "watermark": false,
-            "registrants_email_notification": true
-        },
-        "start_time": "2019-08-30T22:00:00Z",
-        "start_url": "https://zoom.us/1234555466/cdknfdffgggdfg4MDUxNjY0LCJpYXQiOjE1NjgwNDQ0NjQsImFpZCI6IjRBOWR2QkRqVHphd2J0amxoejNQZ1EiLCJjaWQiOiIifQ.Pz_msGuQwtylTtYQ",
-        "status": "waiting",
-        "timezone": "America/New_York",
-        "topic": "My API Test",
-        "type": 2,
-        "uuid": "iAABBBcccdddd7A=="
-        }
-
-
-
-        */
-
-
-
     }
 
 
@@ -318,23 +120,6 @@ class ZoomMeetingAdapter {
             $array = json_decode(json_encode($curl->response), true);
             return $array["invitation"];
         }
-
-
-
-        /*
-
-
-        Example API Response
-
-
-        {
-        "invitation": "Shrijana G is inviting you to a scheduled Zoom meeting.\r\n\r\nTopic: MyTestMeeting\r\nTime: Jul 31, 2019 04:00 PM Pacific Time (US and Canada)\r\n\r\nJoin Zoom Meeting\r\nhttps://zoom.us/j/000000\r\n\r\nOne tap mobile\r\n+000000"
-        }
-
-
-        */
-
-
 
     }
 
@@ -360,55 +145,6 @@ class ZoomMeetingAdapter {
         }
 
 
-        /*
-
-        Example API Response
-
-        {
-        "page_count": 1,
-        "page_number": 1,
-        "page_size": 1,
-        "total_records": 1,
-        "registrants": [
-            {
-            "email": "somemeail@emailprovider.com",
-            "first_name": "Tim",
-            "last_name": "S.",
-            "id": "zjkfsdfjdfhg",
-            "address": "11111 Awesome St",
-            "city": "Paris",
-            "country": "France",
-            "zip": "11000",
-            "state": "Ile-de-France",
-            "phone": "00000",
-            "industry": "Tech",
-            "org": "Growth",
-            "job_title": "Developer",
-            "purchasing_time_frame": "Within a month",
-            "role_in_purchase_process": "Not involved",
-            "no_of_employees": "1-20",
-            "comments": "Love using Zoom APIs",
-            "custom_questions": [
-                {
-                "title": "Did you enjoy the registration process?",
-                "value": "Yes, alot."
-                },
-                {
-                "title": "Would you like to register for our next meeting?",
-                "value": "Absolutely."
-                }
-            ],
-            "status": "approved",
-            "create_time": "2012-04-14T16:55:30.231Z",
-            "join_url": "https://success.zoom.us/j/0000000000000"
-            }
-        ]
-        }
-
-        */
-
-
-
     }
 
 
@@ -425,7 +161,7 @@ class ZoomMeetingAdapter {
 
 
 
-        
+
     }
 
 
