8a181cf4ce31c5798801c91ee62c04e85983b104
Louis Ronald <louisronsonronald@gmail.com>
2021-06-28 23:42:19 +1000

    Tested addRoom in RoomAdapter class on zoom-room.php file

--- tests/test-zoom-room.php
+++ tests/test-zoom-room.php
@@ -3,5 +3,5 @@
 require_once("../zoom-room.php");
 
 $jwtToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6IlFlRnJtOGlVUUVLbUJDZHF0MU9nUlEiLCJleHAiOjE2MjUzMjE2NzgsImlhdCI6MTYyNDcxNjgxNH0.tIot15lt1Ptb6iWeaBYfWttqAX32EWRGzIb3oXm5ZIY";
-$rooms = RoomAdapter::getRooms($jwtToken);
+$rooms = RoomAdapter::addRoom($jwtToken, "Made Via PHP Room", "ZoomRoom");
 echo json_encode($rooms);
